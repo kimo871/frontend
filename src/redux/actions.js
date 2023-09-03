dbb518fc181a4947420f8be8ac55f3da4e024cbb
Kimo <mk5391130@gmail.com>
2023-09-03 20:44:02 +0300

    f

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -50,7 +50,7 @@ const Fetch_Login = (email,password,navigate)=>{
     return (dispatch)=>{
         fetch("https://codsoft-1.onrender.com/auth/SignIn",{
             method:"POST",
-            withCredentials:true,
+            credentials:"include",
             headers:{
                 "Content-Type":"application/json"
             },
